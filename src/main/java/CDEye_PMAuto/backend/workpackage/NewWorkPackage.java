87b660841cb0aa1cfffd5988ee442ff248818064
Jaewhee Seo <jstylesss2@gmail.com>
2021-03-08 17:51:49 -0800

    temp create-wp-screen

--- src/main/java/CDEye_PMAuto/backend/workpackage/NewWorkPackage.java
+++ src/main/java/CDEye_PMAuto/backend/workpackage/NewWorkPackage.java
@@ -6,6 +6,9 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import CDEye_PMAuto.backend.project.ActiveProjectBean;
+import CDEye_PMAuto.backend.project.Project;
+
 /**
  * For creating new work packages and temporarily storing their information.
  */
@@ -13,17 +16,40 @@ import javax.inject.Named;
 @RequestScoped
 public class NewWorkPackage extends WorkPackage implements Serializable {
 
-    @Inject private WorkPackageManager workPackageManager;
-    
-    /**
-     * Used to persist the new work package.
-     */
-    public String add() {
-        WorkPackage wp = new WorkPackage(this);
-        wp.setId(UUID.randomUUID());
-        wp.setLeaf(false);
-        workPackageManager.addWorkPackage(wp);
-        return "testWPs";
-    }
+	@Inject
+	private WorkPackageManager workPackageManager;
+	@Inject
+	ActiveProjectBean apb;
+
+	String parentWpNumber = "";
+
+	/**
+	 * Used to persist the new work package.
+	 */
+	public String add() {
+		Project activeProj = new Project(apb);
+		System.out.println(parentWpNumber);
+		System.out.println(activeProj);
+		WorkPackage[] parentWp = workPackageManager.findWpsByPkgNumAndProj(parentWpNumber, activeProj);
+
+		if (parentWp[0] != null) {
+			System.out.println(parentWp[0]);
+		}
+
+		WorkPackage wp = new WorkPackage(this);
+//        wp.setParentWp(parentWp);
+		wp.setId(UUID.randomUUID());
+		wp.setLeaf(false);
+		workPackageManager.addWorkPackage(wp);
+		return "WPList";
+	}
+
+	public String getParentWpNumber() {
+		return parentWpNumber;
+	}
+
+	public void setParentWpNumber(String parentWpNumber) {
+		this.parentWpNumber = parentWpNumber;
+	}
 
 }
